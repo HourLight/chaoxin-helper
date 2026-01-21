681f74ffc1f85e496bb5bbe238d9b0f75e4654fa
馥靈之鑰 Hour Light 芳香能量調頻 <judyanee@gmail.com>
2026-01-22 00:43:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/webhook', express.raw({ type: '*/*' }), async (req, res) => {
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 // 圖片上傳設定
 const storage = multer.memoryStorage();
 const upload = multer({ 
